4c861d98d519a992a642d4171a635bd18a582467
Antonio Gallego <amturing@gmail.com>
2021-04-30 22:56:06 +0200

    Add files via upload
    docstrings

--- pyErlang.py
+++ pyErlang.py
@@ -1,20 +1,32 @@
-## Originally published under http://wanlinksniper.blogspot.com.es/2014/05/erlang-c-en-python-herramientas-para-el.html
-## A few Python routines I put together one evening to compute traffic intensity, agent occupancy, probability of waiting,
-## average speed of answer (ASA), service level, agents needed and other Erlang-C related stuff.
-## broken link: http://www.mitan.co.uk/erlang/elgcmath.htm
-## wayback machine: https://web.archive.org/web/20181209042241/http://www.mitan.co.uk/erlang/elgcmath.htm
-## Python3 ported
+# Originally published under
+# http://wanlinksniper.blogspot.com.es/2014/05/erlang-c-en-python-herramientas-para-el.html
+# A few Python routines I put together one evening to compute
+# traffic intensity, agent occupancy, probability of waiting,
+# average speed of answer (ASA), service level, agents needed and other Erlang-C related stuff.
+# broken link: http://www.mitan.co.uk/erlang/elgcmath.htm
+# wayback machine: https://web.archive.org/web/20181209042241/http://www.mitan.co.uk/erlang/elgcmath.htm
+# Python3 ported April 2021
 
 from math import pow,factorial,log,exp
 
 def PowerFact(b,e):
-    ## Returns b^e / e! used everywhere else in the model
+    """
+    Returns b^e / e! used everywhere else in the model
+    
+    Parameters:
+        b (int): base
+        e (int): exponent
+    """
     return pow(b,e)/factorial(e)
 
 def erlangC(m,u):
-    ## Returns the probability a call waits.
-    ## m is the agent count
-    ## u is the traffic intensity
+    """
+    Returns the probability a call waits.
+
+    Parameters:
+        m   (int): agent count
+        u (float): traffic intensity
+    """
     p = u/m  ##  agent occupancy
     suma = 0
     for k in range(0,m):
@@ -23,26 +35,38 @@ def erlangC(m,u):
     return erlang
 
 def SLA(m,u,T,target):
-    ## Returns the average speed of answer
-    ## m is the agent count
-    ## u is the traffic intensity
-    ## T is the average call time
-    ## target is the target answer time
+    """
+    Returns the average speed of answer
+    
+    Parameters:
+        m        (int): agent count
+        u      (float): traffic intensity
+        T      (float): average call time
+        target (float): target answer time
+    """
     return (1 - erlangC(m, u) * exp(-(m-u) * (target/T)))
 
 def ASA(m,u,T):
-    ## Returns the average speed of answer (ASA)
-    ## m is the agent counts.
-    ## u is the traffic intensity
-    ## T is the average call time
+    """
+    Returns the average speed of answer (ASA)
+    
+    Parameters:
+        m   (int): agent count
+        u (float): traffic intensity
+        T (float): average call time
+    """
     return erlangC(m, u) * (T/(m-u))
 
 def agentsNeeded(u,T,targetSLA,target):
-    ## Returns the number of agents needed to reach given SLA
-    ## u is the traffic intensity
-    ## T is the average call time
-    ## target is the target answer time
-    ## targetSLA % calls answered under target time
+    """
+    Returns the number of agents needed to reach given SLA
+    
+    Parameters:
+        u         (float): traffic intensity
+        T         (float): average call time
+        target    (float): target answer time
+        targetSLA (float): % representing calls answered under target time
+    """
     level=0
     m=1
     while level<targetSLA:
@@ -52,17 +76,23 @@ def agentsNeeded(u,T,targetSLA,target):
     
 
 def showStats(calls,interval,T,m,target,level):
-    # calls       number of calls in a given time interval
-    # interval    the time interval, in secs (1800 s == 30 minutes)
-    # landa       calls/interval
-    # T           average call duration, in secs
-    # m           number of agents
-    # u=landa*T   traffic intensity
-    # p=u/m       agent occupancy
+    """
+    Prints Erlang related statistics
+
+    Parameters:
+        calls    (int): calls received in a given time interval
+        interval (int): time interval in secs (i.e. 1800s == 30m)
+        T        (int): average call time, in secs
+        m        (int): number of agents
+    
+    Intermediate results:    
+        landa       calls/interval
+        u=landa*T   traffic intensity
+        p=u/m       agent occupancy
+    """
     landa = calls/interval
     u=landa*T      # traffic intensity
     p=u/m          # agent occupancy
-
     print('calls: {}   interval: {}   landa: {:.8f} (l = calls/interval)'.format(calls, interval, landa))
     print('traffic intensity: {:.2f}   agents: {}    agent occupancy: {:.2f}'.format(u,m,p))
     print('ErlangC, Probability of waiting: {:.2f}%'.format(erlangC(m,u)*100))
@@ -71,13 +101,21 @@ def showStats(calls,interval,T,m,target,level):
     print('Agents needed to reach {:.2f}% calls answered in <{} secs: {}'.format(level*100,target,agentsNeeded(u,T,level,target)))
 
 def main():
-    # calls       number of calls in a given time interval
-    # interval    the time interval, in secs (1800 s == 30 minutes)
-    # landa       calls/interval
-    # T           average call duration, in secs
-    # m           number of agents
-    # u=landa*T   traffic intensity
-    # p=u/m       agent occupancy
+    """
+    Runs Erlang tests
+
+    Parameters:
+        calls    (int): calls received in a given time interval
+        interval (int): time interval in secs (i.e. 1800s == 30m)
+        T        (int): average call time, in secs
+        m        (int): number of agents
+    
+    Intermediate results:    
+        landa       calls/interval
+        u=landa*T   traffic intensity
+        p=u/m       agent occupancy
+    """
+
     TESTS = [
         #calls,interval,T,m,target,level
         [360, 1800,  240, 55,   15, 0.70],
